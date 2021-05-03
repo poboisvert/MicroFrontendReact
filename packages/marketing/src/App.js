79866a6e161fb8a9de971031aa32940bf66a17b9
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-05-02 21:54:44 -0400

    S3

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
 
+// Import
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
