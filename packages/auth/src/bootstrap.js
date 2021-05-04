8ed8a62d093236e5f2a9dd8d97021c18fd09622e
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-05-04 16:48:16 -0400

    auth init

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -33,7 +33,7 @@ const mount = (el, { onNavigate, defaultHistory }) => {
 
 // call mount immediately - DEV
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root'); // Selector HTML
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() }); // Fix Route
