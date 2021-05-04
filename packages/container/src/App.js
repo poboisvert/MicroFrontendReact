f091c36f94070bffe3248689cf45e1c8dad19ea7
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-05-03 20:25:37 -0400

    New header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+import { BrowserRouter } from 'react-router-dom';
 
 export default () => {
   return (
-    <div>
-      <h1>Branch Activate</h1>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Branch Activate</h1>
+        <MarketingApp />
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 };
