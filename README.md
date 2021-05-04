38afec5abe25c815e1db6ae2d222ea812f12b9af
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-05-03 22:10:05 -0400

    style CSS correction Material-UI

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # MicroFrontendReact
 
+## General
+
 - Run-Time integrations give the container access to a child app's source code after it is loaded in the browser
 
 - Build-Time integrations give the container access to a child app's source code before it is loaded in the browser
+
+## Demo
+
+URL: https://d3na0jlwxajxpb.cloudfront.net/
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Branch Activate</h1>
-        <MarketingApp />
-        <Header />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <h1>Branch Activate</h1>
+
+          <MarketingApp />
+          <Header />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
